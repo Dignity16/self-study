d28e7b70358ae9f8c8b8d3cdb9adcdd146ffecc2
DESKTOP-KUU96AA\jakel <jaewon.jk@gmail.com>
2023-01-31 18:24:50 +0900

    개인 공부

--- Spring/spring-demo-annotations/src/com/luv2code/springdemo/JavaConfigDemoApp.java
+++ Spring/spring-demo-annotations/src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -1,13 +1,14 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AnnotationDemoApp {
+public class JavaConfigDemoApp {
 
 	public static void main(String[] args) {
 		
 		// read spring config file
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		// get the bean from spring container
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
@@ -20,6 +21,6 @@ public class AnnotationDemoApp {
 		
 		// close the context
 		context.close();
-	}
+	}	
 
 }
--- Spring/spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ Spring/spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,14 +1,65 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 // bean id는 thatSillyCoach로 줌
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach : inside default constructor");
+	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach : inside of doMyStartupStuff()");
+	}
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach : inside of doMyCleanupStuff()");
+	}
+	
+	
+	/*
+	// define a setter method
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach : inside doSomeCrazyStuff() method");
+		fortuneService = theFortuneService;
+	}
+	*/
+
+	/*
+	// HappyFortuneService의 값을 가져와서 담고
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	*/
+	
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	//fortuneService안의 getFortune으로 "Today is your lucky day"의 값을 가져올 수 있음
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
